d2cf9b2bc11c4fa5c0f478e77d117ad4a6c06230
mexsov <151562135+mexsov@users.noreply.github.com>
2024-06-17 14:57:52 +0300

    upd

--- server/models/excursionModel.mjs
+++ server/models/excursionModel.mjs
@@ -3,8 +3,8 @@ import { pool } from "../db/postgresConnection.mjs";
 
 const excursionModel = {
 
-    createExcursionModel: async (excursion) => {
-        const {name, image_data, duration_minutes, rating, price } = excursion;
+    createExcursionModel: async (newExcursion) => {
+        const {name, image_data, duration_minutes, rating, price } = newExcursion;
 
         const result = await pool.query("INSERT INTO create_excursion (name, image_data, duration_minutes, rating, price) VALUES ($1, $2, $3, $4, $5) RETURNING *",
         [name, image_data, duration_minutes, rating, price ]);
